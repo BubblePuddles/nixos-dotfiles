7a8a7e076ccdba08cb843fc9fb697849a6721839
BubblePuddles <logan.baker7562@gmail.com>
2026-07-17 13:39:57 -0500

    Initial commit for nix dotfiles

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+
+  description = "My first flake!";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      lib = nixpkgs.lib;
+    in {
+    nixosConfigurations = {
+      logdog-nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+	modules = [ ./configuration.nix ];
+      };
+    };
+  };
+}
