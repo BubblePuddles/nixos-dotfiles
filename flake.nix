1f527eab53ad0340c6a57657c4a8f4cda1ff1b67
BubblePuddles <logan.baker7562@gmail.com>
2026-07-23 21:00:59 -0500

    Added home-manager

--- flake.nix
+++ flake.nix
@@ -7,6 +7,13 @@
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
 
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+
     zen-browser = {
           url = "github:youwen5/zen-browser-flake";
           inputs.nixpkgs.follows = "nixpkgs";
@@ -16,20 +23,32 @@
 
   };
 
-  outputs = { self, nixpkgs, nixpkgs-xr, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-xr, home-manager, ... }@inputs:
     let
+      system = "x86_64-linux";
       inherit (nixpkgs) lib;
+      pkgs = nixpkgs.legacyPackages.${system}
 #      lib = nixpkgs.lib;
       in {
     nixosConfigurations = {
         logdog-nixos = lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./configuration.nix
+            nixpkgs-xr.nixosModules.nixpkgs-xr
+          ];
+			  };
+      };
+
+    homeConfiguartions = {
+      logdog = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
         modules = [
-          ./configuration.nix
-          nixpkgs-xr.nixosModules.nixpkgs-xr
+          ./home.nix
         ];
-			      };
+			};
     };
+
   };
 }
