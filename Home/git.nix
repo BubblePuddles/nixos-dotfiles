7025c65a07fae5d4f96a70a612054a1d9cca24cd
BubblePuddles <logan.baker7562@gmail.com>
2026-07-30 19:42:07 -0500

    Good Starting point for Github

--- Home/git.nix
+++ Home/git.nix
@@ -1,4 +1,17 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
+
+  programs.git = {
+    enable = true;
+    userName = "BubblesPuddle";
+    userEmail = "logan.baker7562@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+
+
+
+
+    };
 
 
 }
--- Modules/network.nix
+++ Modules/network.nix
@@ -6,7 +6,7 @@
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+ services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
